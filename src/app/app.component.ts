43397fcf0d95219896bdc754f6995ffdf22384ea
Panther360 <vtkhemnar@gmail.com>
2022-10-26 09:16:33 +0530

    Employee List Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular 10 Spring Boot CRUD Full Stack App';
+  title = '';
+  // title = 'Presentee Angular FrontEnd';
 }
--- src/app/employee.ts
+++ src/app/employee.ts
@@ -1,6 +1,21 @@
 export class Employee {
-    id: number;
+
     firstName: string;
     lastName: string;
     emailId: string;
+
+    id: number;
+    employee_name: string;
+    dob: Date;
+    res_address: string;
+    perm_address: string;
+    contact: string;
+    acc_number: string;
+    ifsc: string;
+    esic: string;
+    pt: string;
+    aadhaar: string;
+    pan: string;
+    rate: number;
+
 }
